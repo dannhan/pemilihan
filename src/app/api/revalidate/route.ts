a61edd41cc14a7887402e9992e2df243bbf35ef1
Ahmad Danun Hanani <ahmadanun1@gmail.com>
2024-03-27 20:33:45 +0700

    update webhook

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,15 +1,37 @@
 import { isValidSignature, SIGNATURE_HEADER_NAME } from "@sanity/webhook";
 import { headers } from "next/headers";
 import { revalidatePath } from "next/cache";
+import { revalidateTag } from "next/cache";
+import { type NextRequest, NextResponse } from "next/server";
+import { parseBody } from "next-sanity/webhook";
 
-export async function POST(req: Request) {
-  const headersList = headers();
-  const referer = headersList.get("SIGNATURE_HEADER_NAME");
+export async function POST(req: NextRequest) {
+  try {
+    const { body, isValidSignature } = await parseBody<{
+      _type: string;
+      slug?: string | undefined;
+    }>(req, process.env.SANITY_WEBHOOK_SECRET);
+    console.log({ body, isValidSignature });
 
-  console.log({ headersList, referer });
-  // revalidatePath("/", "layout");
+    if (!isValidSignature) {
+      return new Response("Invalid Signature", { status: 401 });
+    }
 
-  return Response.json({ revalidated: true });
+    if (!body?._type) {
+      return new Response("Bad Request", { status: 400 });
+    }
+
+    revalidateTag(body._type);
+    return NextResponse.json({
+      status: 200,
+      revalidated: true,
+      now: Date.now(),
+      body,
+    });
+  } catch (error: any) {
+    console.error(error);
+    return new Response(error.message, { status: 500 });
+  }
 }
 
 export async function GET() {
