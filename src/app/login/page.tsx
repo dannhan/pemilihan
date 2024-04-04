59b63473be404ade88119a259727104b10a90cba
Ahmad Danun Hanani <ahmadanun1@gmail.com>
2024-04-04 10:19:22 +0700

    remove border on smaller sceen

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,10 +5,10 @@ import { GoogleSignInButton } from "@/components/auth-button";
 export default function Page() {
   return (
     <main className="flex min-h-[100dvh] w-full items-center justify-center">
-      <section className="mt-4 flex flex-col gap-6 rounded-lg border p-6 shadow-md">
+      <section className="mt-4 flex flex-col gap-6 rounded-lg p-6 md:border md:shadow-md">
         <div className="space-y-2 text-center">
           <h1 className="text-2xl font-bold">Login</h1>
-          <p className="text-sm text-muted-foreground">
+          <p className="text-xs md:text-sm text-muted-foreground">
             Anda disarankan membuat akun untuk mengelola polling Anda.
             <br />
             Saat ini, kami hanya menyediakan login melalui akun Google.
