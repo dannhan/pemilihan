96f61179567e6ea115e5ca2150e428c678eda0fc
Ahmad Danun Hanani <ahmadanun1@gmail.com>
2024-04-07 11:20:50 +0700

    style change

--- src/app/(home)/create-polling/page.tsx
+++ src/app/(home)/create-polling/page.tsx
@@ -2,7 +2,7 @@ import { CreatePollingForm } from "@/components/forms/create-polling-form";
 
 export default function Page() {
   return (
-    <main className="mx-auto my-8 min-h-screen max-w-screen-xl px-4">
+    <main className="mx-auto min-h-screen max-w-screen-xl px-4">
       <h1 className="mt-4 text-center text-2xl">Buat Polling Anda Sendiri</h1>
       <section className="mt-4 flex flex-col rounded border p-6 shadow-md">
         <CreatePollingForm />
