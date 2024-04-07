e8e3d08bb6d7ec96cac759553816c758ea887deb
Ahmad Danun Hanani <ahmadanun1@gmail.com>
2024-04-07 11:20:01 +0700

    add new page

---
+++ src/app/(home)/about-us/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main className="mx-auto min-h-screen max-w-screen-xl px-4">
+      <h1 className="mt-4 text-center text-2xl">Tentang Kami</h1>
+    </main>
+  );
+}
