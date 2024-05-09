7f42b57213fc3f53e3927b0bde2fd3baf149bc16
Ahmad Danun Hanani <ahmadanun1@gmail.com>
2024-05-10 03:20:53 +0700

    hello world page

--- src/app/helloworld/page.tsx
+++ src/app/helloworld/page.tsx
@@ -1,9 +1,13 @@
-import { firebaseAdminAuth } from "@/firebase/firebaseAdmin";
+import { firebaseAuth } from "@/firebase/firebase";
+import { signInWithEmailAndPassword, updateProfile } from "firebase/auth";
+
 import { getAuth } from "@/lib/auth";
+import { firebaseAdminAuth } from "@/firebase/firebaseAdmin";
 
 export default async function Page() {
   const session = await getAuth();
-  if (process.env.NODE_ENV === "production" || !session) return <h1>Hello World!</h1>;
+  if (process.env.NODE_ENV === "production" || !session)
+    return <h1>Hello World!</h1>;
 
   const userId = session.user.id;
   firebaseAdminAuth.setCustomUserClaims(userId, { admin: true });
@@ -15,4 +19,20 @@ export default async function Page() {
       <pre>{JSON.stringify(session, null, 2)}</pre>
     </>
   );
+}
+
+async function _() {
+  const email = "";
+  const password = "";
+
+  await signInWithEmailAndPassword(firebaseAuth, email, password).catch(
+    (error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      return <pre>{JSON.stringify({ errorCode, errorMessage }, null, 2)}</pre>;
+    },
+  );
+
+  const user = firebaseAuth.currentUser;
+  if (user) updateProfile(user, { displayName: "Admin" });
 }
